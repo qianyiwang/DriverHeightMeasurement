d3a87c4409b231f364240a60477fa5f8fcecd837
qianyi wang <qianyiwang@qianyis-MacBook-Pro.local>
2017-02-20 00:17:01 -0500

    calculate overall acc

--- wear/src/main/java/com/example/qianyiwang/driverheightmeasurement/MotionService.java
+++ wear/src/main/java/com/example/qianyiwang/driverheightmeasurement/MotionService.java
@@ -27,8 +27,8 @@ public class MotionService extends Service implements SensorEventListener, Messa
     private final String TAG = "MotionService";
     Sensor senAccelerometer, senGravity, senMagnetic;
     SensorManager mSensorManager;
-    float[] acceleration, velocity, mRotationMatrix, acc_last;
-    private float timestamp, last_accZ;
+    float[] velocity, mRotationMatrix, acc_last;
+    private float timestamp, abs_acc;
     private static final float NS2S = 1.0f / 1000000000.0f;
     private float dT, time;
     private float calibrate;
@@ -41,20 +41,14 @@ public class MotionService extends Service implements SensorEventListener, Messa
         super.onCreate();
 
         mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
-        senAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 //        senGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        senMagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        mSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);//adjust the frequency
+        mSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_FASTEST);//adjust the frequency
 //        mSensorManager.registerListener(this, senGravity , SensorManager.SENSOR_DELAY_FASTEST);//adjust the frequency
-        mSensorManager.registerListener(this, senMagnetic , SensorManager.SENSOR_DELAY_FASTEST);
 
-        acceleration = new float[3];
         velocity = new float[3];
         mRotationMatrix = new float[9];
         acc_last = new float[3];
-        acceleration[0] = 0;
-        acceleration[1] = 0;
-        acceleration[2] = 0;
 
         apiClient = new GoogleApiClient.Builder(this)
                 .addApi(Wearable.API)
@@ -78,42 +72,25 @@ public class MotionService extends Service implements SensorEventListener, Messa
 //            gravity[2] = event.values[2];
 //        }
 
-        if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
-            SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
-//            Log.e("rotation", mRotationMatrix[6]+","+mRotationMatrix[7]+","+mRotationMatrix[8]);
-        }
 
         // In this example, alpha is calculated as t / (t + dT),
         // where t is the low-pass filter's time-constant and
         // dT is the event delivery rate.
-        else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+        if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
             if(timestamp!=0){
                 final float alpha = 0.25f;
-
-                acceleration[0] = event.values[0] * mRotationMatrix[0] + event.values[1] * mRotationMatrix[1] + event.values[2] * mRotationMatrix[2];
-                acceleration[1] = event.values[0] * mRotationMatrix[3] + event.values[1] * mRotationMatrix[4] + event.values[2] * mRotationMatrix[5];
-                acceleration[2] = event.values[0] * mRotationMatrix[6] + event.values[1] * mRotationMatrix[7] + event.values[2] * mRotationMatrix[8];
-                acceleration[0] /= SensorManager.GRAVITY_EARTH;
-                acceleration[1] /= SensorManager.GRAVITY_EARTH;
-                acceleration[2] = (acceleration[2] - SensorManager.GRAVITY_EARTH) / SensorManager.GRAVITY_EARTH;
-
-                // using ramp as filter
-//                acc_last[0] = acceleration[0] * alpha + acc_last[0] * (1.0f - alpha);
-//                acc_last[1] = acceleration[1] * alpha + acc_last[1] * (1.0f - alpha);
-//                acc_last[2] = acceleration[2] * alpha + acc_last[2] * (1.0f - alpha);
-//                acceleration[0] = acceleration[0] - acc_last[0];
-//                acceleration[1] = acceleration[1] - acc_last[1];
-//                acceleration[2] = acceleration[2] - acc_last[2];
-
                 // using low pass filter: y[i] := y[i-1] + α * (x[i] - y[i-1])
-                acceleration[2] = last_accZ + alpha * (acceleration[2] - last_accZ);
-                last_accZ = acceleration[2];
+                float acc_x = acc_last[0] + alpha * (event.values[0] - acc_last[0]);
+                float acc_y = acc_last[1] + alpha * (event.values[1] - acc_last[1]);
+                float acc_z = acc_last[2] + alpha * (event.values[2] - acc_last[2]);
+
+                abs_acc = (float) Math.sqrt(acc_x*acc_x+acc_y*acc_y+acc_z*acc_z);
 
             }
         }
         dT = (event.timestamp - timestamp) * NS2S;
         if(GlobalVals.msg!=""){
-            if(i>=60){
+            if(i>30){
 //                if(i==61){
 //                    calibrate = acceleration[2];
 //                }
@@ -122,7 +99,7 @@ public class MotionService extends Service implements SensorEventListener, Messa
                 if(time<=GlobalVals.time_target){
                     GlobalVals.z_v = calculateVelocity(dT);
                     GlobalVals.distance = calculateDistance(dT);
-                    Log.e(TAG,GlobalVals.distance+","+GlobalVals.z_v+","+acceleration[2]);
+                    Log.e(TAG,GlobalVals.distance+","+GlobalVals.z_v+","+abs_acc);
                 }
                 else{
                     initialVals();
@@ -151,7 +128,7 @@ public class MotionService extends Service implements SensorEventListener, Messa
     }
 
     private float calculateVelocity(float t){
-        velocity[2] = velocity[2]+(acceleration[2])*t;
+        velocity[2] = velocity[2]+(abs_acc)*t;
         return velocity[2];
     }
 
