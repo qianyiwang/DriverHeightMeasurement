26ef6ddc1e49bc1223d6eef2e3bdff7fbe601f42
qianyi wang <qianyiwang@qianyis-MBP.local.tld>
2017-03-08 16:09:56 -0500

    add sensor accuracy check

--- wear/src/main/java/com/example/qianyiwang/driverheightmeasurement/MotionService.java
+++ wear/src/main/java/com/example/qianyiwang/driverheightmeasurement/MotionService.java
@@ -48,6 +48,7 @@ public class MotionService extends Service {
         private float[] last_acc = new float[3];
         private float[] rotationMatrix = new float[9];
         private final float alpha = 0.25f;
+        private int accuracy = 0;
 
         MySensorEvent(){
             sensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
@@ -61,8 +62,11 @@ public class MotionService extends Service {
         public void onSensorChanged(SensorEvent sensorEvent) {
 
             if(sensorEvent.sensor == rotationSensor){
-                SensorManager.getRotationMatrixFromVector(rotationMatrix, sensorEvent.values);
-                Log.e(TAG, "rotation: "+rotationMatrix[6] + " " + rotationMatrix[7] + " " + rotationMatrix[8]);
+
+                    SensorManager.getRotationMatrixFromVector(rotationMatrix, sensorEvent.values);
+                    accuracy = sensorEvent.accuracy;
+
+//                Log.e(TAG, "rotation: "+rotationMatrix[6] + " " + rotationMatrix[7] + " " + rotationMatrix[8]);
             }
 
             else if(sensorEvent.sensor==accSensor){
@@ -72,9 +76,47 @@ public class MotionService extends Service {
                     sensorEvent.values[i] = last_acc[i] + alpha * (sensorEvent.values[i] - last_acc[i]);
                     last_acc[i] = sensorEvent.values[i];
                 }
+
                 float[] acc_global = calculateGlobalOrientationValue(sensorEvent.values);
-                Log.e(TAG, "acc_global: "+acc_global[0] + " " + acc_global[1] + " " + acc_global[2]);
+                if(acc_global[2]<0){
+                    acc_global[2] += SensorManager.GRAVITY_EARTH;
+                }
+                else{
+                    acc_global[2] -= SensorManager.GRAVITY_EARTH;
+                }
+                acc_global = calibrationFilter(acc_global);
+                Log.e(TAG, "acc_global: "+acc_global[0] + " " + acc_global[1] + " " + acc_global[2] + "accuracy: "+accuracy);
             }
+        }
+
+        private float[] calibrationFilter(float[] data){
+            float[] mSensorBias = {0,0,0};
+            float[] mSensorDelta = {0,0,0};
+            final float[] SENSOR_BIAS_STEP = {0.01f,0.01f,0.02f};
+            final float[] SENSOR_ZERO_RANGE = {0.2f,0.2f,0.4f};
+            for (int i=0; i<3; i++) {
+                mSensorDelta[i] = data[i]-mSensorBias[i];
+                if (mSensorDelta[i] > 0) {
+                    if (mSensorDelta[i] > SENSOR_BIAS_STEP[i]) {
+                        mSensorBias[i] += SENSOR_BIAS_STEP[i];
+                    } else {
+                        mSensorBias[i] += data[i];
+                    }
+                }
+                else {
+                    if (mSensorDelta[i] < -SENSOR_BIAS_STEP[i]) {
+                        mSensorBias[i] -= SENSOR_BIAS_STEP[i];
+                    } else {
+                        mSensorBias[i] -= data[i];
+                    }
+                }
+                mSensorDelta[i] = data[i]-mSensorBias[i];
+
+                if ((mSensorDelta[i] < SENSOR_ZERO_RANGE[i]) && (mSensorDelta[i] > -SENSOR_ZERO_RANGE[i])) {
+                    mSensorDelta[i] = 0;
+                }
+            }
+            return mSensorDelta;
         }
 
         private float[] calculateGlobalOrientationValue(float[] data){
